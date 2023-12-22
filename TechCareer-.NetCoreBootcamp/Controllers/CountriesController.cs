eb2c540914f6132020fdc81eb775d11952e72f41
Alper Özkul <alper.ozkul@bil.omu.edu.tr>
2023-12-22 21:09:16 +0300

    Add project files.

---
+++ TechCareer-.NetCoreBootcamp/Controllers/CountriesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechCareer_.NetCoreBootcamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly string[] countries = {"Türkiye",
+                "ABD",
+                "Rusya",
+                "Almanya",
+                "Fransa",
+                "İtalya",
+                "Çin",
+                "Hindistan",
+                "Kanada",
+                "Danimarka",
+                "Finlandiya",
+                "İsveç",
+                "İspanya",
+                "İrlanda",
+                "Japonya",
+                "Katar",
+                "Litvanya",
+                "Macaristan",
+                "Polonya",
+                "Romanya"
+            };
+        [HttpGet]
+        public string[] Get()
+        {           
+            return countries;
+        }
+
+
+        [HttpGet("{totalCountries}")]
+        public string[] Get(int totalCountries)
+        {
+            var selectedCountries = countries.Take(totalCountries).ToArray();
+            return selectedCountries;
+        }
+
+    }
+
+    
+
+}
