b3874e8abb4da3f73dbc98af77437e49388f3217
Alper Özkul <alper.ozkul@bil.omu.edu.tr>
2023-12-23 20:43:43 +0300

    Basic demo (CRD operations)

---
+++ TechCareer-.NetCoreBootcamp/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechCareer_.NetCoreBootcamp.Models;
+using TechCareer_.NetCoreBootcamp.Services;
+
+namespace TechCareer_.NetCoreBootcamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        static List<Product> products;
+
+        public ProductController()
+        {
+            if (products == null) products = ProductService.GetAllProduct();
+        }
+
+        [HttpGet]
+        public IActionResult getAll()
+        {
+            return Ok(products);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult getById(int id)
+        {
+            var product = products.FirstOrDefault(x => x.Id == id);
+            if (product == null) return NotFound();
+            else return Ok(product);
+
+        }
+
+
+        [HttpPost]
+        public IActionResult create(Product product)
+        {
+            product.Id = products.Count + 1;
+            products.Add(product);
+            return StatusCode(StatusCodes.Status201Created, product);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult delete(int id)
+        {
+            var product = products.FirstOrDefault(x => x.Id == id);
+            if (product == null) return NotFound();
+            else
+            {
+                products.Remove(product);
+                return Ok(product);
+            }
+        }
+    }
+}
---
+++ TechCareer-.NetCoreBootcamp/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace TechCareer_.NetCoreBootcamp.Models
+{
+    public class Product
+    {
+        public Product(int id, string name, decimal unitPrice, Category category)
+        {
+            Id = id;
+            Name = name;
+            UnitPrice = unitPrice;
+            Category = category;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+        public Category Category { get; set; }
+    }
+}
