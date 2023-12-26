451c4469cdc4ac1320fd4091c449d732f81a1753
Alper Özkul <alper.ozkul@bil.omu.edu.tr>
2023-12-26 21:48:04 +0300

    Added Entity Framework, Created Db, Added basic Employee CRUD operations.

---
+++ TechCareer-.NetCoreBootcamp/Context/TechCareerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechCareer_.NetCoreBootcamp.Models.ORM;
+
+namespace TechCareer_.NetCoreBootcamp.Context
+{
+    public class TechCareerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=TechCareerDb;Trusted_Connection=True; TrustServerCertificate=True");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ TechCareer-.NetCoreBootcamp/Models/ORM/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechCareer_.NetCoreBootcamp.Models.ORM
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Ad boş geçilemez.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Soyad boş geçilemez.")]
+        public string LastName { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }   
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
